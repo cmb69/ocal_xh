6b614bd15ced53dbbd9a9199b426177c74fe444b
cmb69 <cmbecker69@gmx.de>
2014-08-08 23:33:33 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,14 @@
+<?php
+
+$plugin_tx['ocal']['date_months']="Januar,Februar,März,April,Mai,Juni,Juli,August,September,Oktober,November,Dezember";
+$plugin_tx['ocal']['date_days']="Mo,Di,Mi,Do,Fr,Sa,So";
+$plugin_tx['ocal']['label_prev_year']="Voriges&nbsp;Jahr";
+$plugin_tx['ocal']['label_prev_month']="Voriger&nbsp;Monat";
+$plugin_tx['ocal']['label_today']="Heute";
+$plugin_tx['ocal']['label_next_month']="Nächster&nbsp;Monat";
+$plugin_tx['ocal']['label_next_year']="Nächstes&nbsp;Jahr";
+$plugin_tx['ocal']['label_save']="Speichern";
+$plugin_tx['ocal']['message_saved']="Erfolgreich gespeichert.";
+$plugin_tx['ocal']['alt_logo']="Kalender";
+
+?>
