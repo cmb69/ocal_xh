f5819a20acebcf741a7d2fe52be5f460362aeff2
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-05 00:43:04 +0100

    Use an anonymous function for autoloading

--- classes/required_classes.php
+++ classes/required_classes.php
@@ -5,15 +5,9 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  */
 
-/**
- * @param string $class
- */
-function Ocal_autoload($class)
-{
+spl_autoload_register(function ($class) {
     $parts = explode('\\', $class, 2);
     if ($parts[0] == 'Ocal') {
         include_once dirname(__FILE__) . '/' . $parts[1] . '.php';
     }
-}
-
-spl_autoload_register('Ocal_autoload');
+});
