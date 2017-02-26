0750cde4cc919f4be31ef9262b8cd5908c854ef5
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-26 15:27:16 +0100

    Switch to GNU style file headers

--- classes/required_classes.php
+++ classes/required_classes.php
@@ -1,8 +1,22 @@
 <?php
 
 /**
- * @copyright 2014-2017 Christoph M. Becker <http://3-magi.net/>
- * @license http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * Copyright 2014-2017 Christoph M. Becker
+ *
+ * This file is part of Ocal_XH.
+ *
+ * Ocal_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocal_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ocal_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 spl_autoload_register(function ($class) {
--- ocal.js
+++ ocal.js
@@ -1,9 +1,20 @@
 /*!
- * Ocal_XH
+ * Copyright 2014-2017 Christoph M. Becker
  *
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014-2017 Christoph M. Becker <http://3-magi.net/>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * This file is part of Ocal_XH.
+ *
+ * Ocal_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocal_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ocal_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*jslint browser: true, maxlen: 80 */
