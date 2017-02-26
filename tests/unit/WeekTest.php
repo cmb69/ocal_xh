c88a0e85784b66f32e66b9c13329e0eeb28dcc0d
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-27 00:35:01 +0100

    Put Week under test

---
+++ tests/unit/WeekTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Copyright 2014-2017 Christoph M. Becker
+ *
+ * This file is part of Ocal_XH.
+ *
+ * Ocal_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocal_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ocal_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Ocal;
+
+use PHPUnit_Framework_TestCase;
+
+class WeekTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Week
+     */
+    private $subject;
+
+    public function setUp()
+    {
+        $this->subject = new Week(9, 2017);
+    }
+
+    public function testGetWeek()
+    {
+        $this->assertSame(9, $this->subject->getWeek());
+    }
+
+    public function testGetYear()
+    {
+        $this->assertSame(2017, $this->subject->getYear());
+    }
+
+    public function testGetIso()
+    {
+        $this->assertSame('2017-09', $this->subject->getIso());
+    }
+
+    /**
+     * @dataProvider provideGetNextWeekData
+     * @param int $offset
+     * @param string $iso
+     */
+    public function testGetNextWeek($offset, $iso)
+    {
+        $this->assertSame($iso, $this->subject->getNextWeek($offset)->getIso());
+    }
+
+    /**
+     * @return array
+     */
+    public function provideGetNextWeekData()
+    {
+        return array(
+            [-9, '2016-52'],
+            [-8, '2017-01'],
+            [ 0, '2017-09'],
+            [ 1, '2017-10'],
+            [43, '2017-52'],
+            [44, '2018-01']
+        );
+    }
+
+    public function testGetNextWeekDefaut()
+    {
+        $this->assertSame('2017-10', $this->subject->getNextWeek()->getIso());
+    }
+}
