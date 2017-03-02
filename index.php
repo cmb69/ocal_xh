84a29f648ff87161c3727208821f2c5085db77d4
Christoph M. Becker <cmbecker69@gmx.de>
2017-03-02 19:11:44 +0100

    Remove ugly version check
    After we're having implemented a proper system check, there's no need
    for the early version check in index.php anymore; direct access
    prevention is superfluous anyway.

--- index.php
+++ index.php
@@ -19,23 +19,6 @@
  * along with Ocal_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Prevent direct access and usage from unsupported CMSimple_XH versions.
- */
-if (!defined('CMSIMPLE_XH_VERSION')
-    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
-    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6', 'lt')
-) {
-    header('HTTP/1.1 403 Forbidden');
-    header('Content-Type: text/plain; charset=UTF-8');
-    die(
-        <<<EOT
-Ocal_XH detected an unsupported CMSimple_XH version.
-Uninstall Ocal_XH or upgrade to a supported CMSimple_XH version!
-EOT
-    );
-}
-
 class_alias('Ocal\Occupancy', 'Ocal_Occupancy');
 class_alias('Ocal\HourlyOccupancy', 'Ocal_HourlyOccupancy');
 
