cmb69/ocal_xh

commit 81f78ec951a75d4a05a32dff6942e61f2d21aed5

Close #10: Add more states
Instead of hard-coding the maximum state to 3, we make it configurable
and support an arbitrary amount of states. Users have to manually add
respective state labels and colors, though.

Also, we slightly change the logic of the list views to show entries
for all states except the zero state, regardless whether they are
labeled.

classes/Occupancy.php
@@ -21,8 +21,6 @@
 
 namespace Ocal;
 
-use Serializable;
-
 class Occupancy
 {
     /**
@@ -36,6 +34,11 @@ class Occupancy
     protected $states;
 
     /**
+     * @var int
+     */
+    private $maxState;
+
+    /**
      * @param string $name
      * @param string $json
      * @return ?Occupancy
@@ -43,18 +46,30 @@ class Occupancy
     public static function createFromJson($name, $json)
     {
         $array = json_decode($json, true);
-        switch ($array['type']) {
+        if (!($result = self::instantiateType($array['type'], $name))) {
+            return null;
+        }
+        foreach ($array['states'] as $date => $state) {
+            $result->setState($date, $state);
+        }
+        return $result;
+    }
+
+    /**
+     * @param string $type
+     * @param string $name
+     * @return Occupancy
+     */
+    private static function instantiateType($type, $name)
+    {
+        switch ($type) {
             case 'daily':
-                $result = new Occupancy($name);
-                break;
+                return new Occupancy($name);
             case 'hourly':
-                $result = new HourlyOccupancy($name);
-                break;
+                return new HourlyOccupancy($name);
             default:
                 return null;
         }
-        $result->states = $array['states'];
-        return $result;
     }
 
     /**
@@ -62,8 +77,11 @@ class Occupancy
      */
     public function __construct($name)
     {
+        global $plugin_cf;
+
         $this->name = (string) $name;
         $this->states = array();
+        $this->maxState = (int) $plugin_cf['ocal']['state_max'];
     }
 
     /**
@@ -100,11 +118,10 @@ class Occupancy
      */
     protected function getState($date)
     {
-        if (isset($this->states[$date])) {
-            return $this->states[$date];
-        } else {
+        if (!isset($this->states[$date])) {
             return 0;
         }
+        return $this->states[$date];
     }
 
     /**
@@ -113,7 +130,7 @@ class Occupancy
      */
     public function setState($date, $state)
     {
-        if ($state) {
+        if ($state >= 0 && $state <= $this->maxState) {
             $this->states[$date] = $state;
         } else {
             unset($this->states[$date]);

classes/CalendarController.php
@@ -200,7 +200,7 @@ abstract class CalendarController
     protected function prepareToolbarView()
     {
         $view = new View('toolbar');
-        $view->states = range(0, 3);
+        $view->states = range(0, $this->config['state_max']);
         return $view;
     }
 

classes/ListService.php
@@ -121,8 +121,8 @@ class ListService
     {
         $result = [];
         foreach ($list as $range => $state) {
-            $label = $this->lang["label_state_$state"];
-            if ($label) {
+            if ($state) {
+                $label = $this->lang["label_state_$state"];
                 $result[] = (object) compact('range', 'state', 'label');
             }
         }

config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+$plugin_cf['ocal']['state_max']="3";
+
 $plugin_cf['ocal']['hour_first']="7";
 $plugin_cf['ocal']['hour_interval']="1";
 $plugin_cf['ocal']['hour_last']="19";

css/stylesheet.css
@@ -47,19 +47,19 @@
 .ocal_state {
     border: 1px solid #888;
 }
-[data-ocal_state="0"] {
+*[data-ocal_state="0"] {
     background: white;
     color: black;
 }
-[data-ocal_state="1"] {
+*[data-ocal_state="1"] {
     background: green;
     color: white;
 }
-[data-ocal_state="2"] {
+*[data-ocal_state="2"] {
     background: yellow;
     color: black;
 }
-[data-ocal_state="3"] {
+*[data-ocal_state="3"] {
     background: red;
     color: white;
 }

help/help.htm
@@ -31,6 +31,7 @@ href="http://frankziesing.de/cmsimple/en/?Welcome:Calendar">Calendar</a>.</p>
 <li><a href="#usage">Usage</a>
 <ul>
 <li><a href="#usage-admin">Administration</a></li>
+<li><a href="#usage-states">States</a></li>
 </ul>
 </li>
 <!--<li><a href="#limitations">Limitations</a></li>-->
@@ -112,17 +113,53 @@ are displayed. When you're logged in as administrator, you'll see a toolbar
 where you can pick one of the available states by clicking it. Then you can
 click individual dates in the displayed calendars to assign them this state.
 When you're finished, you have to save your modifications.</p>
-<p>Four states are available, which have the following default meanings:</p>
+<p>By default, four states are available which have the following meaning:</p>
 <ul>
 <li>0 (white): non applicable</li>
 <li>1 (green): available</li>
 <li>2 (yellow): reserved or partly available</li>
 <li>3 (red): (fully) booked out</li>
 </ul>
-<p>If required, you can change the colors in the stylesheet and the labels in
-the language settings.</p>
+<p>
+    Note that the number and meaning of the states <a href="#usage-states">is
+    customizable</a>.
+</p>
 <p>Note that the administration requires JavaScript and a somewhat contemporary
 browser (e.g. IE < 8 is not supported).</p>
+<div class="toplink"><a href="#">back to top</a></div>
+
+<h3 id="usage-states">States</h3>
+<p>
+    It is possible to have an arbitrary amount of states, each with its own
+    meaning. The maximum state that is available has to be set in the
+    configuration (State → Max). Note that in addition to that many states,
+    there is also state 0, which is the default state that is not shown in the
+    list views.
+</p>
+<p>
+    You can customize the state labels in the language settings of the plugin.
+    If you have increased State → Max, you have to manually edit the respective
+    language files in <code>plugins/ocal/languages/</code>, and have to add
+    further state labels, like so:
+</p>
+<pre>
+    $plugin_tx['ocal']['label_state_4']="extra state";
+</pre>
+<p>
+    You can customize the state colors in the plugin's stylesheet, and add rules
+    for additional states like so:
+</p>
+<pre>
+    *[data-ocal_state="4"] {
+        background: blue;
+        color: white;
+    }
+</pre>
+<p>
+    To summarize: set the maximum state in the configuration, add (or remove)
+    the state labels in the language file and add (or remove) state color rules
+    in the stylesheet.
+</p>
 <div class="toplink"><a href="#">back to top</a></div>
 
 <!--<h2 id="limitations">Limitations</h2>

help/help_de.htm
@@ -33,6 +33,7 @@ werden.</p>
 <li><a href="#usage">Verwendung</a>
 <ul>
 <li><a href="#usage-admin">Administration</a></li>
+<li><a href="#usage-states">Zustände</a></li>
 </ul>
 </li>
 <!--<li><a href="#limitations">Beschränkungen</a></li>-->
@@ -123,17 +124,56 @@ Werkzeugleiste, wo Sie einen der verfügbaren Zustände durch Anklicken auswähl
 können. Dann können Sie einzelne Tage in den angezeigten Kalendern anklicken, um
 diesen den gewählten Zustand zuzuweisen. Wenn Sie damit fertig sind, müssen Sie
 Ihre Änderungen speichern.</p>
-<p>Es sind vier Zustände verfügbar, die wie folgt vorbelegt sind:</p>
+<p>Standardmäßig sind vier Zustände verfügbar, die wie folgt vorbelegt sind:</p>
 <ul>
 <li>0 (weiß): nicht anwendbar</li>
 <li>1 (grün): verfügbar</li>
 <li>2 (gelb): reserviert oder teilweise verfügbar</li>
 <li>3 (rot): (komplett) ausgebucht</li>
 </ul>
-<p>Bei Bedarf können Sie die Farben im Stylesheet und die Beschriftungen in den
-Spracheinstellungen ändern.</p>
+<p>
+    Es ist möglich Anzahl und Bedeutung der Zustände <a
+    href="#usage-states">anzupassen.</a>
+</p>
 <p>Beachten Sie, dass die Administration JavaScript und einen einigermaßen
 zeitgemäßen Browser erfordert (z.B. wird IE < 8 nicht unterstüzt).</p>
+<div class="toplink"><a href="#">zum Seitenanfang</a></div>
+
+<h3 id="usage-states">Zustände</h3>
+<p>
+    Es kann eine beliebige Anzahl von Zuständen verwendet werden, wobei jeder
+    eine eigene Bedeutung hat. Der höchste Zustand, der verfügbar sein soll,
+    muss in der Konfiguration (State → Max) eingestellt werden. Es ist zu
+    beachten, dass zusätzlich zu dieser Zustandsanzahl noch der 0 Zustand
+    existiert, der der Vorgabezustand ist, der nicht in den Listenansichten
+    angezeigt wird.
+</p>
+<p>
+    Die Zustandsbeschriftungen können in den Spracheinstellungen des Plugins
+    angepasst werden. Wurde State → Max erhöht, müssen die verwendeten
+    Sprachdatein in <code>plugins/ocal/languages/ manuell</code> bearbeitet
+    werden, wo weitere Zustandsbeschriftungen etwa wie folgt hinzugefügt werden
+    müssen:
+</p>
+<pre>
+    $plugin_tx['ocal']['label_state_4']="weiterer Zustand";
+</pre>
+<p>
+    Die Zustandsfarben können im Stylesheet des Plugins angepasst werden, wo
+    Regeln für weitere Zustände wie folgt ergänzt werden können:
+</p>
+<pre>
+    *[data-ocal_state="4"] {
+        background: blue;
+        color: white;
+    }
+</pre>
+<p>
+    Zusammenfassung: der höchste Zustand wird in der Konfiguration festgelegt,
+    Zustandsbeschriftungen werden in der Sprachdatei hinzugefügt (oder
+    entfernt), und Zustandsfarbregeln werden im Stylesheet hinzugefügt (oder
+    entfernt).
+</p>
 <div class="toplink"><a href="#">zum Seitenanfang</a></div>
 
 <!--<h2><a name="limitations">Beschränkungen</a></h2>-->

languages/de.php
@@ -31,6 +31,7 @@ $plugin_tx['ocal']['syscheck_writable']="'%s' schreibbar ist";
 $plugin_tx['ocal']['syscheck_xhversion']="die CMSimple_XH-Version ≥ %s";
 $plugin_tx['ocal']['alt_logo']="Kalender";
 
+$plugin_tx['ocal']['cf_state_max']="Der höchste Zustand, der unterstüzt wird. Details sind im Abschnitt \"Zustände\" des Handbuchs zu finden.";
 $plugin_tx['ocal']['cf_hour_first']="Die erste Stunde, die in den stündlichen Belegungskalendern angezeigt werden soll.";
 $plugin_tx['ocal']['cf_hour_interval']="Das ganzzahlige stündliche Intervall. Soll beispielsweise nur jede zweite Stunde angezeigt werden, geben Sie \"2\" ein.";
 $plugin_tx['ocal']['cf_hour_last']="Die letzte Stunde, die in den stündlichen Belegungskalendern angezeigt werden soll.";

commit a5eeebf871e4afd25c061855de9ae7dd1a2c37ac

Fix: Zero states are stored
This regression has been introduced with commit 81f78ec, and is
actually a one-off error.

classes/Occupancy.php
@@ -130,7 +130,7 @@ class Occupancy
      */
     public function setState($date, $state)
     {
-        if ($state >= 0 && $state <= $this->maxState) {
+        if ($state > 0 && $state <= $this->maxState) {
             $this->states[$date] = $state;
         } else {
             unset($this->states[$date]);